7df7c217ffed863fa29d619aef358ee20f1a7d9f
alaanabil45 <alaanab2005@gmail.com>
2025-08-25 16:13:18 +0200

    home Second section completed

--- src/app/features/home/home/home.ts
+++ src/app/features/home/home/home.ts
@@ -3,10 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FirestoreService } from '../../auth/firestore.service';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
