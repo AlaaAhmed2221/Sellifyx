b31ec79dcb31790499bfd126653a3f5667741173
hanaeldaly54 <hanaeldaly68@gmail.com>
2025-08-27 13:21:45 +0300

    products page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,11 @@ export const routes: Routes = [
   { path: 'home', loadComponent: () => import('./features/home/home/home').then(m => m.Home) },
   { path: 'auth/login', loadComponent: () => import('./features/auth/login/login').then(m => m.Login) },
   { path: 'auth/register', loadComponent: () => import('./features/auth/register/register').then(m => m.Register) },
-  { path: '**', redirectTo: '' }
+  
+  {
+  path: 'product/:id',
+loadComponent: () => import('./features/product/product').then(m => m.ProductComponent)
+},
+{ path: 'product/:id', loadComponent: () => import('./features/product/product').then(m => m.ProductComponent) },
+{ path: '**', redirectTo: '' },
 ];
